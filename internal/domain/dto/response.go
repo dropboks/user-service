00889bfe5d51b82e1c46f1249376cc54f773d2dd
daffadon <daffaputranarendra9@gmail.com>
2025-06-10 14:52:24 +0700

    feat(dto): add get id and profile response dto

--- internal/domain/dto/response.go
+++ internal/domain/dto/response.go
@@ -5,7 +5,17 @@ import "errors"
 var (
 	Err_INTERNAL_FAILED_BUILD_QUERY = errors.New("failed to build query")
 	Err_INTERNAL_FAILED_SCAN_USER   = errors.New("failed to scan user")
-	Err_INTERNAL_FAILED_INSERT_USER  = errors.New("failed to scan user")
+	Err_INTERNAL_FAILED_INSERT_USER = errors.New("failed to scan user")
 
 	Err_NOTFOUND_USER_NOT_FOUND = errors.New("user not found")
+
+	Err_UNAUTHORIZED_USER_ID_NOTFOUND = errors.New("user_id is not found")
+)
+
+type (
+	GetProfileResponse struct {
+		FullName string `json:"full_name"`
+		Image    string `json:"image"`
+		Email    string `json:"email"`
+	}
 )
