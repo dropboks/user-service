9c1a1d92859694607ff4978eb361b7d6581cb69d
daffadon <daffaputranarendra9@gmail.com>
2025-06-10 14:47:23 +0700

    refactory(repository): change filename to entity base repository

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -15,24 +15,25 @@ import (
 )
 
 type (
-	AuthRepository interface {
-		QueryUserByEmail(string) (*entity.User, error)
+	UserRepository interface {
 		CreateNewUser(*entity.User) error
+		QueryUserByEmail(string) (*entity.User, error)
+		QueryUserByUserId(string) (*entity.User, error)
 	}
-	authRepository struct {
+	userRepository struct {
 		pgx    *pgxpool.Pool
 		logger zerolog.Logger
 	}
 )
 
-func NewAuthRepository(pgx *pgxpool.Pool, logger zerolog.Logger) AuthRepository {
-	return &authRepository{
+func NewUserRepository(pgx *pgxpool.Pool, logger zerolog.Logger) UserRepository {
+	return &userRepository{
 		pgx:    pgx,
 		logger: logger,
 	}
 }
 
-func (a *authRepository) CreateNewUser(user *entity.User) error {
+func (a *userRepository) CreateNewUser(user *entity.User) error {
 	query, args, err := sq.Insert("users").
 		Columns("id", "full_name", "image", "email", "password").
 		Values(user.ID, user.FullName, user.Image, user.Email, user.Password).
@@ -51,7 +52,33 @@ func (a *authRepository) CreateNewUser(user *entity.User) error {
 	return nil
 }
 
-func (a *authRepository) QueryUserByEmail(email string) (*entity.User, error) {
+func (a *userRepository) QueryUserByUserId(userId string) (*entity.User, error) {
+	var user entity.User
+	query, args, err := sq.Select("id", "full_name", "image", "email", "password").
+		From("users").
+		Where(sq.Eq{"id": userId}).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		a.logger.Error().Err(err).Msg("failed to build query")
+		return nil, dto.Err_INTERNAL_FAILED_BUILD_QUERY
+	}
+	row := a.pgx.QueryRow(context.Background(), query, args...)
+	err = row.Scan(&user.ID, &user.FullName, &user.Image, &user.Email, &user.Password)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			a.logger.Warn().Str("id", userId).Msg("user not found")
+			return nil, dto.Err_NOTFOUND_USER_NOT_FOUND
+		}
+		a.logger.Error().Err(err).Msg("failed to scan user")
+		return nil, dto.Err_INTERNAL_FAILED_SCAN_USER
+	}
+	return &user, nil
+
+}
+
+func (a *userRepository) QueryUserByEmail(email string) (*entity.User, error) {
 	var user entity.User
 	query, args, err := sq.Select("id", "full_name", "image", "email", "password").
 		From("users").
