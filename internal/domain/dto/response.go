bc2c6474acf4cf7e10e7a3a6bc35814383356172
daffadon <daffaputranarendra9@gmail.com>
2025-06-25 09:06:42 +0700

    refactor(type): add new error message and update response struct

--- internal/domain/dto/response.go
+++ internal/domain/dto/response.go
@@ -5,7 +5,8 @@ import "errors"
 var (
 	Err_INTERNAL_FAILED_BUILD_QUERY = errors.New("failed to build query")
 	Err_INTERNAL_FAILED_SCAN_USER   = errors.New("failed to scan user")
-	Err_INTERNAL_FAILED_INSERT_USER = errors.New("failed to scan user")
+	Err_INTERNAL_FAILED_INSERT_USER = errors.New("failed to insert user")
+	Err_INTERNAL_FAILED_UPDATE_USER = errors.New("failed to update user")
 
 	Err_NOTFOUND_USER_NOT_FOUND = errors.New("user not found")
 
@@ -14,8 +15,10 @@ var (
 
 type (
 	GetProfileResponse struct {
-		FullName string `json:"full_name"`
-		Image    string `json:"image"`
-		Email    string `json:"email"`
+		FullName         string `json:"full_name"`
+		Image            string `json:"image"`
+		Email            string `json:"email"`
+		Verified         bool   `json:"verified"`
+		TwoFactorEnabled bool   `json:"two_factor_enabled"`
 	}
 )
