7d6ec58ec8f97b9eba12ce247e303cfefd3cdaa4
daffadon <daffaputranarendra9@gmail.com>
2025-06-26 14:39:02 +0700

    refactor(struct): add struct for update request and new message response

--- internal/domain/dto/request.go
+++ internal/domain/dto/request.go
@@ -1,7 +1,16 @@
 package dto
 
+import "mime/multipart"
+
 type (
 	UserData struct {
 		UserId string `json:"user_id"`
+	}
+
+	UpdateUserRequest struct {
+		FullName         string                `form:"full_name"`
+		Image            *multipart.FileHeader `form:"image"`
+		Email            string                `form:"email"`
+		TwoFactorEnabled bool                  `form:"two_factor_enabled"`
 	}
 )
--- internal/domain/dto/response.go
+++ internal/domain/dto/response.go
@@ -3,14 +3,23 @@ package dto
 import "errors"
 
 var (
+	SUCCESS_GET_PROFILE    = "success get profile data"
+	SUCCESS_UPDATE_PROFILE = "success update profile data"
+)
+
+var (
 	Err_INTERNAL_FAILED_BUILD_QUERY = errors.New("failed to build query")
 	Err_INTERNAL_FAILED_SCAN_USER   = errors.New("failed to scan user")
 	Err_INTERNAL_FAILED_INSERT_USER = errors.New("failed to insert user")
 	Err_INTERNAL_FAILED_UPDATE_USER = errors.New("failed to update user")
+	Err_INTERNAL_CONVERT_IMAGE      = errors.New("error processing image")
 
 	Err_NOTFOUND_USER_NOT_FOUND = errors.New("user not found")
 
 	Err_UNAUTHORIZED_USER_ID_NOTFOUND = errors.New("user_id is not found")
+
+	Err_BAD_REQUEST_WRONG_EXTENTION     = errors.New("error file extension, support jpg, jpeg, and png")
+	Err_BAD_REQUEST_LIMIT_SIZE_EXCEEDED = errors.New("max size exceeded: 6mb")
 )
 
 type (
