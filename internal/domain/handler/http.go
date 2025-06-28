e4d13edb37e462c43e85f89c8ed40621b6eb6d11
daffadon <daffaputranarendra9@gmail.com>
2025-06-28 21:06:30 +0700

    feat(route): add change email and password route

--- internal/domain/handler/http.go
+++ internal/domain/handler/http.go
@@ -4,7 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func RegisterUserRoutes(r *gin.Engine, uh UserHandler) *gin.Engine {
 	{
-		r.PATCH("",uh.UpdateUser)
+		r.PATCH("", uh.UpdateUser)
+		r.PATCH("/email", uh.ChangeEmail)
+		r.PATCH("/password", uh.ChangePassword)
 		r.GET("/me", uh.GetProfile)
 	}
 	return r
