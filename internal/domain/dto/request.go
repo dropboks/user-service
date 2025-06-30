d7b0766fe2f26e8e314ae4dc85f9979adb786f0f
daffadon <daffaputranarendra9@gmail.com>
2025-06-30 10:58:57 +0700

    refactor(types): add validation to input

--- internal/domain/dto/request.go
+++ internal/domain/dto/request.go
@@ -8,7 +8,7 @@ type (
 	}
 
 	UpdateUserRequest struct {
-		FullName         string                `form:"full_name"`
+		FullName         string                `form:"full_name" binding:"required,min=1,max=100"`
 		Image            *multipart.FileHeader `form:"image"`
 		TwoFactorEnabled bool                  `form:"two_factor_enabled"`
 	}
