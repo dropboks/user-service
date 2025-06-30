6fe4f9972f7b316be2a4c3c9ea2ba32b889ddc4d
daffadon <daffaputranarendra9@gmail.com>
2025-06-30 16:19:30 +0700

    refactor(di): add initilization for nats, jetstream, and redis

--- cmd/di/container.go
+++ cmd/di/container.go
@@ -1,12 +1,15 @@
 package di
 
 import (
+	"github.com/dropboks/user-service/config/cache"
 	"github.com/dropboks/user-service/config/database"
 	"github.com/dropboks/user-service/config/logger"
+	mq "github.com/dropboks/user-service/config/message-queue"
 	"github.com/dropboks/user-service/config/router"
 	"github.com/dropboks/user-service/internal/domain/handler"
 	"github.com/dropboks/user-service/internal/domain/repository"
 	"github.com/dropboks/user-service/internal/domain/service"
+	_cache "github.com/dropboks/user-service/internal/infrastructure/cache"
 	"github.com/dropboks/user-service/internal/infrastructure/grpc"
 	"go.uber.org/dig"
 )
@@ -19,6 +22,18 @@ func BuildContainer() *dig.Container {
 	if err := container.Provide(database.New); err != nil {
 		panic("Failed to provide database: " + err.Error())
 	}
+	if err := container.Provide(mq.New); err != nil {
+		panic("Failed to provide nats connection: " + err.Error())
+	}
+	if err := container.Provide(mq.NewJetstream); err != nil {
+		panic("Failed to provide jetstream connection: " + err.Error())
+	}
+	if err := container.Provide(cache.New); err != nil {
+		panic("Failed to provide cache client: " + err.Error())
+	}
+	if err := container.Provide(_cache.New); err != nil {
+		panic("Failed to provide cache infrastructure: " + err.Error())
+	}
 	if err := container.Provide(grpc.NewGRPCClientManager); err != nil {
 		panic("Failed to provide GRPC Client Manager: " + err.Error())
 	}
@@ -27,6 +42,9 @@ func BuildContainer() *dig.Container {
 	}
 	if err := container.Provide(repository.NewUserRepository); err != nil {
 		panic("Failed to provide authRepository: " + err.Error())
+	}
+	if err := container.Provide(repository.NewRedisRepository); err != nil {
+		panic("Failed to provide cache client: " + err.Error())
 	}
 	if err := container.Provide(service.NewAuthService); err != nil {
 		panic("Failed to provide auth service: " + err.Error())
